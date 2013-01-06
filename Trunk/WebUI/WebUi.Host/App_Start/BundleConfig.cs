8cc2fec34c14bfa7781cd83dc34aa42c9b4c1997
KRB <esko22@gmail.com>
2013-01-06 12:31:56 -0800

    adding in twitter bootstrap, jquery update and ko

--- Trunk/WebUI/WebUi.Host/App_Start/BundleConfig.cs
+++ Trunk/WebUI/WebUi.Host/App_Start/BundleConfig.cs
@@ -5,39 +5,42 @@ namespace WebUi.Host
 {
     public class BundleConfig
     {
-        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            // Force optimization to be on or off, regardless of web.config setting
+            BundleTable.EnableOptimizations = false;
+            bundles.UseCdn = false;
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                        "~/Scripts/jquery-ui-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.unobtrusive*",
-                        "~/Scripts/jquery.validate*"));
+            // .debug.js, -vsdoc.js and .intellisense.js files 
+            // are in BundleTable.Bundles.IgnoreList by default.
+            // Clear out the list and add back the ones we want to ignore.
+            // Don't add back .debug.js.
+            bundles.IgnoreList.Clear();
+            bundles.IgnoreList.Ignore("*-vsdoc.js");
+            bundles.IgnoreList.Ignore("*intellisense.js");
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Scripts/libs/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/libs/jquery-{version}.js"));
 
-            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-                        "~/Content/themes/base/jquery.ui.core.css",
-                        "~/Content/themes/base/jquery.ui.resizable.css",
-                        "~/Content/themes/base/jquery.ui.selectable.css",
-                        "~/Content/themes/base/jquery.ui.accordion.css",
-                        "~/Content/themes/base/jquery.ui.autocomplete.css",
-                        "~/Content/themes/base/jquery.ui.button.css",
-                        "~/Content/themes/base/jquery.ui.dialog.css",
-                        "~/Content/themes/base/jquery.ui.slider.css",
-                        "~/Content/themes/base/jquery.ui.tabs.css",
-                        "~/Content/themes/base/jquery.ui.datepicker.css",
-                        "~/Content/themes/base/jquery.ui.progressbar.css",
-                        "~/Content/themes/base/jquery.ui.theme.css"));
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/libs/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jsextlibs").Include(
+                        "~/Scripts/libs/knockout-{version}.js",
+                        "~/Scripts/libs/bootstrap.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jsapplibs")
+                    .IncludeDirectory("~/Scripts/app/", "*.js", searchSubdirectories: false));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                        "~/Content/bootstrap.css",
+                        "~/Content/bootstrap-responsive.css"));
+
         }
     }
 }
